f383aa2eb616f041d25a3f3ea4aaf5c5500abed0
Jumair Ahmad <37107267+jumairahmad@users.noreply.github.com>
2024-04-01 18:48:34 +0500

    telegrambot in python

--- main.py
+++ main.py
@@ -1,16 +1,64 @@
 # This is a sample Python script.
+import os
+import telebot
+import yfinance as yf
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
 
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+  bot.reply_to(message, "Hey! Hows it going?")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@bot.message_handler(commands=['hello'])
+def hello(message):
+  bot.send_message(message.chat.id, "Hello!")
 
+@bot.message_handler(commands=['wsb'])
+def get_stocks(message):
+  response = ""
+  stocks = ['gme', 'amc', 'nok']
+  stock_data = []
+  for stock in stocks:
+    data = yf.download(tickers=stock, period='2d', interval='1d')
+    data = data.reset_index()
+    response += f"-----{stock}-----\n"
+    stock_data.append([stock])
+    columns = ['stock']
+    for index, row in data.iterrows():
+      stock_position = len(stock_data) - 1
+      price = round(row['Close'], 2)
+      format_date = row['Date'].strftime('%m/%d')
+      response += f"{format_date}: {price}\n"
+      stock_data[stock_position].append(price)
+      columns.append(format_date)
+    print()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+  response = f"{columns[0] : <10}{columns[1] : ^10}{columns[2] : >10}\n"
+  for row in stock_data:
+    response += f"{row[0] : <10}{row[1] : ^10}{row[2] : >10}\n"
+  response += "\nStock Data"
+  print(response)
+  bot.send_message(message.chat.id, response)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def stock_request(message):
+  request = message.text.split()
+  if len(request) < 2 or request[0].lower() not in "price":
+    return False
+  else:
+    return True
+
+@bot.message_handler(func=stock_request)
+def send_price(message):
+  request = message.text.split()[1]
+  data = yf.download(tickers=request, period='5m', interval='1m')
+  if data.size > 0:
+    data = data.reset_index()
+    data["format_date"] = data['Datetime'].dt.strftime('%m/%d %I:%M %p')
+    data.set_index('format_date', inplace=True)
+    print(data.to_string())
+    bot.send_message(message.chat.id, data['Close'].to_string(header=False))
+  else:
+    bot.send_message(message.chat.id, "No data!?")
+
+bot.polling()
